4f6360a743b2bc871febb683226787676c8cb459
jiyun0522 <wnlemforhs24@gmail.com>
2025-11-04 14:27:10 +0900

    ✨ Feat: ShortLink 및 QRCode CRUD 기능 구현

---
+++ pages/api/short-links/[id].js
@@ -0,0 +1,27 @@
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+
+export default async function handler(req, res) {
+  await dbConnect();
+  const { id } = req.query;
+
+  switch (req.method) {
+    case "PATCH":
+      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).send(updatedShortLink);
+      break;
+    case "GET":
+      const shortLink = await ShortLink.findById(id);
+      res.send(shortLink);
+      break;
+    case "DELETE":
+      await ShortLink.findByIdAndDelete(req.query.id);
+      res.status(204).send();
+      break;
+    default:
+      res.send();
+      break;
+  }
+}
